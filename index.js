b9d20d922aab7d85ca4b5dc2b7aec18cc7598025
duman <duman.fabian.g@gmail.com>
2022-06-29 14:01:47 -0500

    deploy

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const routerApi = require('./routes');
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+app.use(cors(options));
+
+app.get('/', (req, res) => {
+  res.send('Hola');
+});
+
+app.get('/new-route', (req, res) => {
+  res.send('New route');
+});
+
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+
+app.listen(port, () => {
+  console.log('Port: ' + port);
+});
